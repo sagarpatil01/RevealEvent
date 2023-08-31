e1fbc29b62d1bf0ce2ba50007d4aff7fbb5c4e7b
Your Name <you@example.com>
2023-08-31 11:02:51 +0530

    DotNet Rest API

---
+++ RE_DOTNet/AdminRestAPI/Models/Event.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdminRestAPI.Models;
+
+public partial class Event
+{
+    public int Id { get; set; }
+
+    public string EventName { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+
+    public DateOnly EventDate { get; set; }
+
+    public string Duration { get; set; } = null!;
+
+    public decimal Price { get; set; }
+
+    public int? GenreCatId { get; set; }
+
+    public int? VenueId { get; set; }
+
+    public int? OrganiserId { get; set; }
+
+    public virtual ICollection<EventArtist> EventArtists { get; set; } = new List<EventArtist>();
+
+    public virtual GenreCategory? GenreCat { get; set; }
+
+    public virtual Organizer? Organiser { get; set; }
+
+    public virtual ICollection<TicketBooking> TicketBookings { get; set; } = new List<TicketBooking>();
+
+    public virtual Venue? Venue { get; set; }
+}
---
+++ RE_DOTNet/AdminRestAPI/Models/GenreCategory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdminRestAPI.Models;
+
+public partial class GenreCategory
+{
+    public int Id { get; set; }
+
+    public string CategoryName { get; set; } = null!;
+
+    public string? CategoryType { get; set; }
+
+    public virtual ICollection<Artist> Artists { get; set; } = new List<Artist>();
+
+    public virtual ICollection<Event> Events { get; set; } = new List<Event>();
+}
